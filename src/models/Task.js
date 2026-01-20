9fcf9b8b771c80d144b028a3e6cf51cb25940f3a
maksim2012520 <maksim2012520@gmail.com>
2026-01-20 21:04:18 +0700

    Update Task.js

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
-const taskSchema = new mongoose.Schema({
+const TaskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
-  createdAt: { type: Date, default: Date.now }
-});
+  dueDate: Date,
+  category: String,
+  completed: Boolean,
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true });
 
-module.exports = mongoose.model('Task', taskSchema);
+module.exports = mongoose.model('Task', TaskSchema);
